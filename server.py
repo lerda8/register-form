7c6e187be7027874b93d7184d0219e00f8da50c9
Lucas Ballaty <lucas.ballaty@gmail.com>
2023-03-14 21:34:28 +0100

    first commit

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app. route("/")
+def index():
+    return render_template('index.html')
+
+
+@app. route( '/login')
+def login():
+    return render_template('login.html')
+
+
+@app. route('/register')
+def register():
+    return render_template('register.html')
+
+
