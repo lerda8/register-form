3a7ccbd61a8c5d53e14101cdc71461bf1ae12568
Lucas Ballaty <lucas.ballaty@gmail.com>
2023-03-15 22:32:38 +0100

    Almost working

--- server.py
+++ server.py
@@ -1,27 +1,47 @@
+import sqlite3
 from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
+def get_connection():
+    conn = sqlite3.connect('users.db')
+    return conn
 
-@app. route("/")
+def create_user_tables():
+    db = get_connection()
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS users
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        password TEXT,
+        admin INTEGER)""")
+
+create_user_tables()
+
+@app.route("/")
 def index():
     return render_template('index.html')
 
-
-@app. route( '/login')
-def login():
+@app.route('/login/form')
+def login_form():
     return render_template('login.html')
 
-
-@app. route('/register')
-def register():
+@app.route('/register/form', methods=["POST", "GET"])
+def register_form():
     return render_template('register.html')
 
-@app.route("/dashboard/<username>")
-def dashboard(username=None):
-    return render_template("dashboard.html", username=username)
+@app.route("/register", methods=["POST", "GET"])
+def new_account():
+    db = get_connection()
+    name = request.form["name"]
+    password = request.form["password"]
+    password1 = request.form["password1"]
+    if password == password1:
+        db.execute("INSERT INTO users (username, password) VALUES (?, ?)", (name, password))
+        db.commit()
+        return "THANK YOU FOR REGISTERING"
+    else:
+        return "THE PASSWORDS ARE NOT MATCHING, TRY AGAIN"
 
-@app.route("/admin_dashboard/<username>")
-def accoadmin_dashboard(username=None):
-    return render_template("admin_dashboard.html", username=username)
+
