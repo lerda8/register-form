30a3d02c7e398ab4a453d8ecaa4fbdf7bce3cbfd
Lucas Ballaty <lucas.ballaty@gmail.com>
2023-03-16 15:03:44 +0100

    final version for now

--- server.py
+++ server.py
@@ -4,8 +4,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 def get_connection():
     conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
     return conn
 
 def create_user_tables():
@@ -31,33 +33,49 @@ def login_form():
 def register_form():
     return render_template('register.html')
 
+
+
+
 @app.route("/register/admin", methods=["POST", "GET"])
 def new_account():
-    db = get_connection()
+    connection = get_connection()
+    cursor = connection.cursor()
     name = request.form["name"]
     password = request.form["password"]
     password1 = request.form["password1"]
-    if password == password1:
-        db.execute("INSERT INTO users (username, password, admin) VALUES (?, ?, ?)", (name, password, "1"))
-        db.commit()
+    query = "SELECT username FROM users where username='"+name+"'"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    if password == password1 and len(results) == 0:
+        connection.execute("INSERT INTO users (username, password, admin) VALUES (?, ?, ?)", (name, password, "1"))
+        connection.commit()
         return render_template("admin_user.html", name=name)
-    else:
+    if password != password1 and len(results) > 0: 
+        return "THE PASSWORDS ARE NOT MATCHING AND AN ACCOUNT WITH THIS USERNAME ALREADY EXISTS"
+    if password != password1:
         return "THE PASSWORDS ARE NOT MATCHING, TRY AGAIN"
+    else:
+        return "AN ACCOUNT WITH THIS USERNAME ALREADY EXISTS"
+
+
     
 @app.route("/register/reg_account", methods=["POST", "GET"])
 def new_reg_account():
-    db = get_connection()
+    connection = get_connection()
+    cursor = connection.cursor()
     name = request.form["name"]
     password = request.form["password"]
     password1 = request.form["password1"]
-    if password == password1:
-        db.execute("INSERT INTO users (username, password, admin) VALUES (?, ?, ?)", (name, password, "0"))
-        db.commit()
+    query = "SELECT username FROM users where username='"+name+"'"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    if password == password1 and len(results) == 0:
+        connection.execute("INSERT INTO users (username, password, admin) VALUES (?, ?, ?)", (name, password, "0"))
+        connection.commit()
         return render_template("reg_user.html", name=name)
-    else:
+    if password != password1 and len(results) > 0: 
+        return "THE PASSWORDS ARE NOT MATCHING AND AN ACCOUNT WITH THIS USERNAME ALREADY EXISTS"
+    if password != password1:
         return "THE PASSWORDS ARE NOT MATCHING, TRY AGAIN"
-    
-
-    
-
-
+    else:
+       return "AN ACCOUNT WITH THIS USERNAME ALREADY EXISTS"
