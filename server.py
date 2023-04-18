63da4eba182e197e6a6b3d881c1d11f721f2238c
Lucas Ballaty <lucas.ballaty@gmail.com>
2023-04-18 14:25:23 +0200

    Membership added

--- server.py
+++ server.py
@@ -17,9 +17,70 @@ def create_user_tables():
         (id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT,
         password TEXT,
-        admin INTEGER)""")
+        admin TEXT,
+        plan INTEGER DEFAULT 0,
+        blocked INTEGER NOT NULL DEFAULT 0)""")
 
 create_user_tables()
+
+def create_project_tables():
+    db = get_connection()
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS projects
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE)""")
+
+create_project_tables()
+
+def create_task_tables():
+    db = get_connection()
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS tasks
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        description TEXT NOT NULL,
+        due_date DATE NOT NULL,
+        is_completed INTEGER NOT NULL DEFAULT 0,
+        project_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE,
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE)""")
+
+create_task_tables()
+
+def create_files_tables():
+    db = get_connection()
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS files
+        (id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        path TEXT NOT NULL,
+        task_id INTEGER NOT NULL,
+        FOREIGN KEY (task_id) REFERENCES tasks (id) ON DELETE CASCADE)""")
+
+create_files_tables()
+
+
+@app.route('/change-plan', methods=['POST'])
+def change_plan():
+    user_id = request.form['user_id']
+    full_access = str(session['full_access'])
+    if full_access == "True":
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute('UPDATE users SET plan = ? WHERE username = ?', (0, user_id))
+        connection.commit()
+        session['full_access'] = False
+        return redirect(url_for('dashboard', username=user_id, full_access=session['full_access']))
+    else:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute('UPDATE users SET plan = ? WHERE username = ?', (1, user_id))
+        connection.commit()
+        session['full_access'] = True
+        return redirect(url_for('dashboard', username=user_id, full_access=session['full_access']))
 
 @app.route("/")
 def index():
@@ -29,38 +90,92 @@ def index():
 def login_form():
     return render_template('login.html')
 
-@app.route("/login/form/sucess", methods=["POST", "GET"])
+@app.route("/login/form/success", methods=["POST", "GET"])
 def login_go():
     connection = get_connection()
     cursor = connection.cursor()
     name = request.form["name"]
     password = request.form["password"]
     query_login = connection.execute('SELECT * FROM users WHERE username = ?', (name,)).fetchone()
-    results=list(query_login)
-    if name in results and password == results[2] and results[3] == 1:
-        session.clear()
-        session['user_id'] = results[1]
-        if results[3] == 1:
-            user_id = session.get('user_id')
-            return redirect(url_for('dashboard', username=user_id))
-        else:
-            return "regular login"
-    if len(results) <= 0: 
+    if query_login is None:
         return "WRONG EMAIL OR PASSWORD"
+    results = list(query_login)
+    if name in results and password == results[2]:
+        session.clear()
+        session['user_plan'] = results[4]
+        session['user_id'] = results[1]
+        if results[3] == 'yes':
+            if results[4] == 0:
+                session['full_access'] = False
+                session['is_admin'] = True
+                full_access = session.get('full_access')
+                user_id = session.get('user_id')
+                return redirect(url_for('dashboard',username=user_id, full_access=full_access))
+            else:
+                session['full_access'] = True
+                session['is_admin'] = True
+                full_access = session.get('full_access')
+                user_id = session.get('user_id')
+                return redirect(url_for('dashboard',username=user_id, full_access=full_access))
+        else:
+            if results[4] == 0:
+                session['full_access'] = False
+                session['is_admin'] = False
+                full_access = session.get('full_access')
+                user_id = session.get('user_id')
+                return redirect(url_for('dashboard',username=user_id, full_access=full_access))
+            else:
+                session['full_access'] = True
+                session['is_admin'] = False
+                full_access = session.get('full_access')
+                user_id = session.get('user_id')
+                return redirect(url_for('dashboard',username=user_id, full_access=full_access))
     else:
-        return "WRONG EMAIL OR PASSWORD" 
-    
-@app.route('/dashboard/<username>')
+        return "ERROR"
+
+
+@app.route("/dashboard/<username>", methods=["GET"])
 def dashboard(username):
-    return render_template("dashboard.html")
+    if 'user_id' in session:
+        is_admin = session.get('is_admin')
+        user_type = 'Administrator' if is_admin else 'Regular User'
+        connection = get_connection()
+        cursor = connection.cursor()
+        query = 'SELECT * FROM users'
+        users = cursor.execute(query).fetchall()
+        connection.close()
+        return render_template('dashboard.html', username=username, user_type=user_type, users=users)
+    else:
+        return redirect(url_for('login_form'))
 
 
+@app.route("/delete_account", methods=["POST"])
+def delete_account():
+    user_id = session.get('user_id')
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute('DELETE FROM users WHERE username=?', (user_id,))
+    connection.commit()
+    session.clear()
+    return redirect(url_for('index'))
 
-@app.route('/logout')
+@app.route('/dashboard/delete/<int:user_id>', methods=['POST'])
+def delete_user(user_id):
+    if 'user_id' in session and session.get('is_admin'):
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute('DELETE FROM users WHERE id=?', (user_id,))
+        connection.commit()
+        connection.close()
+        username = session.get('user_id')
+        return redirect(url_for('dashboard', username=username))
+    else:
+        return redirect(url_for('login_form'))
+
+@app.route("/logout", methods=["POST"])
 def logout():
     session.clear()
     return render_template("logout.html")
-    
 
 @app.route('/register/form', methods=["POST", "GET"])
 def register_form():
@@ -77,7 +192,7 @@ def new_account():
     cursor.execute(query)
     results = cursor.fetchall()
     if password == password1 and len(results) == 0:
-        connection.execute("INSERT INTO users (username, password, admin) VALUES (?, ?, ?)", (name, password, "1"))
+        connection.execute("INSERT INTO users (username, password, admin) VALUES (?, ?, ?)", (name, password, "yes"))
         connection.commit()
         return render_template("admin_user.html", name=name)
     if password != password1 and len(results) > 0: 
@@ -98,7 +213,7 @@ def new_reg_account():
     cursor.execute(query)
     results = cursor.fetchall()
     if password == password1 and len(results) == 0:
-        connection.execute("INSERT INTO users (username, password, admin) VALUES (?, ?, ?)", (name, password, "0"))
+        connection.execute("INSERT INTO users (username, password, admin) VALUES (?, ?, ?)", (name, password, "no"))
         connection.commit()
         return render_template("reg_user.html", name=name)
     if password != password1 and len(results) > 0: 
